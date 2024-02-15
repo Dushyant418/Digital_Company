e8fac434e188f5ddeeaf5bd87a78526fa2599f0b
Dushyant418 <dushyantlochab335@gmail.com>
2024-02-15 16:43:25 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'DMsans': ['DM sans']
+      },
+      boxShadow: {
+        'card_shadow': '0px 20px 50px 0px #3B3E430D'
+      }
+    },
+  },
+  plugins: [],
+}
+
