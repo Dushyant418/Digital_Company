1dfe5caa1fd1387dcd33efaa5765887fc8953d5a
Dushyant418 <dushyantlochab335@gmail.com>
2024-02-16 16:16:32 +0530

    _final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,11 @@ module.exports = {
         'DMsans': ['DM sans']
       },
       boxShadow: {
-        'card_shadow': '0px 20px 50px 0px #3B3E430D'
+        'card_shadow': '0px 20px 50px 0px #3B3E430D',
+        'hover_box': '0px 4px 10px 2px #a963fb',
+        'white_shadow': '0px 4px 10px 2px #ffffff'
       }
+      // shadow- [0px_4px_10px_2px_#fffff]
     },
   },
   plugins: [],
